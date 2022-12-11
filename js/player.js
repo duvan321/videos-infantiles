836a9359f5f0662a56c8e43936cf16be0dc82c34
Diego patiño <diegoduvan321@gmail.com>
2022-12-11 10:24:21 -0500

    modifique js cambiando los titulos de los videos

--- js/player.js
+++ js/player.js
@@ -1,6 +1,6 @@
 window.onload=inicio;
-  var videos=["Los Pinguinos de Madagascar_ Escenas Graciosas.mp4","Superlibro - El hijo Pródigo.mp4","Superlibro - Eliseo y los Sirio.mp4","Superlibro - Jonás.mp4","Superlibro - La Primera Navidad.mp4","Superlibro - Los Milagros de Jesús.mp4","Superlibro - En el Principio.mp4"];
-  var vid;
+  var videos=["Los Pinguinos de Madagascar_ Escenas Graciosas.mp4","El Pájaro Loco _ Gallina carpintera _ Episodio Completo.mp4","Toy Story Toons_ Vacaciones en Hawai.mp4","Eso es asqueroso _ Clip de Los Pingüinos de Madagascar.mp4"];
+   var vid;
   var videoActual=0; 
 function inicio(){
     vid=document.querySelector("video");
