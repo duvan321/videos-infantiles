a3136b0272723156c002a3472f52f9220c3b82b8
Diego patiño <diegoduvan321@gmail.com>
2022-12-11 19:50:29 -0500

    los videos en javascript

--- js/player.js
+++ js/player.js
@@ -1,5 +1,5 @@
 window.onload=inicio;
-  var videos=["Los Pinguinos de Madagascar_ Escenas Graciosas.mp4","El Pájaro Loco _ Gallina carpintera _ Episodio Completo.mp4","Toy Story Toons_ Vacaciones en Hawai.mp4","Eso es asqueroso _ Clip de Los Pingüinos de Madagascar.mp4"];
+  var videos=["Los Pinguinos de Madagascar_ Escenas Graciosas.mp4","El Pájaro Loco _ Gallina carpintera _ Episodio Completo.mp4","Toy Story Toons_ Vacaciones en Hawai.mp4","Eso es asqueroso _ Clip de Los Pingüinos de Madagascar.mp4","Con los Pájaros Equivocados _ Los Pingüinos de Madagascar.mp4","Algo falta _ Los Pingüinos de Madagascar.mp4"];
    var vid;
    var speed=2;
    var orden=[];
